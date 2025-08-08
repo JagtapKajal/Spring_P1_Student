cd4b524308fe6f22ccfb3533d86357ed4f549b5e
jagtapK <jagtapkajal54@gmail.com>
2025-08-08 23:17:35 +0530

    create new method

--- src/main/java/com/college/springP1Student/repository/StudentRepository.java
+++ src/main/java/com/college/springP1Student/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.college.springP1Student.repository;
 import com.college.springP1Student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+import java.util.List;
 
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    List<Student> findByCity(String city);
 }
