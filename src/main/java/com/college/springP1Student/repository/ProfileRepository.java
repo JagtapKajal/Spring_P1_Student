96c9050de18c0eb6e88b4879687167fe41f42266
jagtapK <jagtapkajal54@gmail.com>
2025-08-17 11:43:57 +0530

    fix repo

--- src/main/java/com/college/springP1Student/repository/ProfileRepository.java
+++ src/main/java/com/college/springP1Student/repository/ProfileRepository.java
@@ -1,6 +1,6 @@
 package com.college.springP1Student.repository;
 
-import org.springframework.context.annotation.Profile;
+import com.college.springP1Student.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {}
