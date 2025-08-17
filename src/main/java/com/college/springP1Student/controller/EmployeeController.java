3d0e03a3febcb4a605700d137c6591ea182f48d8
jagtapK <jagtapkajal54@gmail.com>
2025-08-17 22:22:10 +0530

    Perform CRUD operation with mapping

--- src/main/java/com/college/springP1Student/controller/EmployeeController.java
+++ src/main/java/com/college/springP1Student/controller/EmployeeController.java
@@ -14,22 +14,32 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    public EmployeeController(EmployeeService employeeService){
+    public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
+    // Create User + Profile
     @PostMapping("/addEmp")
-    public Employee addEmployee(@RequestBody Employee employee){
+    public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmp(employee);
     }
 
+    // Get User by ID (with Profile)
     @GetMapping("/getEmployee/{id}")
-    public Employee getById(@PathVariable int id){
+    public Employee getById(@PathVariable int id) {
         return employeeService.getById(id);
     }
 
+    // Get All Users
     @GetMapping
-    public List<Employee> getAllEmployee(){
+    public List<Employee> getAllEmployee() {
         return employeeService.getAllEmp();
     }
+
+    // Delete User by ID (Profile will also be deleted due to cascade)
+    @DeleteMapping("/DeleteEmp/{id}")
+    public String deleteById(@PathVariable int id) {
+        return employeeService.DeleteById(id);
+    }
+
 }
