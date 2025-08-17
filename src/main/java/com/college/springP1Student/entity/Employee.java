e52235e23b4508083f63692ae37f8b7feca742bf
jagtapK <jagtapkajal54@gmail.com>
2025-08-17 10:50:07 +0530

    Create Employee class with details

--- src/main/java/com/college/springP1Student/entity/Employee.java
+++ src/main/java/com/college/springP1Student/entity/Employee.java
@@ -1,4 +1,42 @@
 package com.college.springP1Student.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String empname;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "passport_id", referencedColumnName = "id")
+    private Passport passport;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
 }
