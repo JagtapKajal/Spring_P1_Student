96aedd6ccd06507da4f7fb1009f6f3d75776b6eb
jagtapK <jagtapkajal54@gmail.com>
2025-08-18 22:34:07 +0530

    Write getter setter to perform one to many

--- src/main/java/com/college/springP1Student/entity/Department.java
+++ src/main/java/com/college/springP1Student/entity/Department.java
@@ -1,8 +1,34 @@
 package com.college.springP1Student.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String deptName;
 
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Students> student;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
 }
