4f3560505cdf996ad4bfec9d67d5c35e04d3a9a3
jagtapK <jagtapkajal54@gmail.com>
2025-08-18 23:03:36 +0530

    Add method

--- src/main/java/com/college/springP1Student/service/DepartmentService.java
+++ src/main/java/com/college/springP1Student/service/DepartmentService.java
@@ -1,8 +1,19 @@
 package com.college.springP1Student.service;
 
 import com.college.springP1Student.entity.Department;
+import com.college.springP1Student.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
 
-public interface DepartmentService {
+@Service
+public class DepartmentService {
 
-    Department saveDep(Department department);
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
 }
