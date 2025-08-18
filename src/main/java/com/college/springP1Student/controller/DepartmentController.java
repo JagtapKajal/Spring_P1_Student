04f48e1601d4112d6908c431bdf6134ccff34245
jagtapK <jagtapkajal54@gmail.com>
2025-08-18 23:12:36 +0530

    Add method to get department

--- src/main/java/com/college/springP1Student/controller/DepartmentController.java
+++ src/main/java/com/college/springP1Student/controller/DepartmentController.java
@@ -3,10 +3,9 @@ package com.college.springP1Student.controller;
 import com.college.springP1Student.entity.Department;
 import com.college.springP1Student.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Department")
@@ -21,5 +20,10 @@ public class DepartmentController {
     @PostMapping("/addDep")
     public Department createDepartment(@RequestBody Department department) {
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/getDep")
+    public List<Department> getDepartments() {
+        return departmentService.getAllDepartments();
     }
 }
